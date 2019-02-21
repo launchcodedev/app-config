2b212c71eeac438936bcd5f350d7829f1b8db99a
Joel Gallant <joel@joelgallant.me>
2019-02-20 22:45:05 -0700

    Moves JSON schema logic into its own function

--- src/generate.ts
+++ src/generate.ts
@@ -1,10 +1,11 @@
 import { join, basename, extname } from 'path';
 import { outputFile } from 'fs-extra';
-import { loadConfig } from './config';
-import { loadSchema } from './schema';
+import { loadConfig, ConfigObject } from './config';
+import { loadSchema, SchemaRefs } from './schema';
 import { loadMeta, resetMetaProps } from './meta';
 import {
   quicktype,
+  RendererOptions,
   JSONSchema,
   JSONSchemaInput,
   JSONSchemaStore,
@@ -34,68 +35,81 @@ export const generateTypeFiles = async (cwd = process.cwd()) => {
 
   const { generate = [] } = meta;
 
+  // default to PascalCase with non-word chars removed
+  const normalizeName = (file: string) => basename(file, extname(file))
+      .split(/[^\w]/)
+      .map(s => `${s.charAt(0).toUpperCase()}${s.slice(1)}`)
+      .join('');
+
   await Promise.all(generate.map(async ({
     file,
     type = extname(file).slice(1),
-    name,
+    name = normalizeName(file),
     augmentModule = true,
     leadingComments,
     rendererOptions = {},
   }) => {
-    // default to PascalCase with non-word chars removed
-    const normalizeName = (file: string) => basename(file, extname(file))
-        .split(/[^\w]/)
-        .map(s => `${s.charAt(0).toUpperCase()}${s.slice(1)}`)
-        .join('');
-
-    if (!name) {
-      name = normalizeName(file); /* tslint:disable-line */
-    }
-
-    const src: JSONSchemaSourceData = {
-      name,
-      schema: JSON.stringify(schema),
-    };
-
-    class FetchingJSONSchemaStore extends JSONSchemaStore {
-      async fetch(address: string): Promise<JSONSchema | undefined> {
-        return (schemaRefs as any)[address];
-      }
-    }
-
-    const inputData = new InputData();
-    await inputData.addSource(
-      'schema',
-      src,
-      () => new JSONSchemaInput(new FetchingJSONSchemaStore(), []),
+    const lines = await generateQuicktype(
+      schema, schemaRefs, file, type, name, augmentModule, leadingComments, rendererOptions,
     );
-
-    const { lines } = await quicktype({
-      inputData,
-      lang: type,
-      indentation: '  ',
-      leadingComments: leadingComments || [
-        'AUTO GENERATED CODE',
-        'Run app-config with \'generate\' command to regenerate this file',
-      ],
-      rendererOptions: {
-        'just-types': 'true',
-        'runtime-typecheck': 'false',
-        ...rendererOptions,
-      },
-    });
-
-    if (type === 'ts' && augmentModule !== false) {
-      lines.push(...[
-        '// augment the default export from app-config',
-        "declare module '@servall/app-config' {",
-        `  export interface ExportedConfig extends ${name} {}`,
-        '}',
-      ]);
-    }
 
     await outputFile(join(cwd, file), `${lines.join('\n')}${'\n'}`);
   }));
 
   return generate;
 };
+
+const generateQuicktype = async (
+  schema: ConfigObject,
+  schemaRefs: SchemaRefs | undefined,
+  file: string,
+  type: string,
+  name: string,
+  augmentModule: boolean,
+  leadingComments: string[] | undefined,
+  rendererOptions: RendererOptions,
+) => {
+  const src: JSONSchemaSourceData = {
+    name,
+    schema: JSON.stringify(schema),
+  };
+
+  class FetchingJSONSchemaStore extends JSONSchemaStore {
+    async fetch(address: string): Promise<JSONSchema | undefined> {
+      return (schemaRefs as any)[address];
+    }
+  }
+
+  const inputData = new InputData();
+  await inputData.addSource(
+    'schema',
+    src,
+    () => new JSONSchemaInput(new FetchingJSONSchemaStore(), []),
+  );
+
+  const { lines } = await quicktype({
+    inputData,
+    lang: type,
+    indentation: '  ',
+    leadingComments: leadingComments || [
+      'AUTO GENERATED CODE',
+      'Run app-config with \'generate\' command to regenerate this file',
+    ],
+    rendererOptions: {
+      'just-types': 'true',
+      'runtime-typecheck': 'false',
+      ...rendererOptions,
+    },
+  });
+
+  if (type === 'ts' && augmentModule !== false) {
+    lines.push(...[
+      '// augment the default export from app-config',
+      "declare module '@servall/app-config' {",
+      `  export interface ExportedConfig extends ${name} {}`,
+      '}',
+    ]);
+  }
+
+  return lines;
+}
