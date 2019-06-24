0662cd4e70cf4728aaaafe4f529cb79458e22325
Joel Gallant <joel@joelgallant.me>
2019-06-24 14:56:59 -0600

    Adds APP_CONFIG_DISABLE flag

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { loadValidatedSync } from './schema';
 
-const { config } = loadValidatedSync();
+let config;
+
+if (process.env.APP_CONFIG_DISABLE !== '1') {
+  config = loadValidatedSync().config;
+} else {
+  config = undefined as any;
+}
 
 // the config type that is exported to consumers and can be augmented
 export interface ExportedConfig {}
