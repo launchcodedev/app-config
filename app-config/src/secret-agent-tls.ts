e9a8941182483c463f624888e617ac8ee61a653d
Joel Gallant <joel@joelgallant.me>
2020-11-18 13:46:22 -0700

    fix: lint

--- app-config/src/secret-agent-tls.ts
+++ app-config/src/secret-agent-tls.ts
@@ -88,7 +88,7 @@ export async function generateCert(): Promise<Cert> {
 export async function loadOrCreateCert(): Promise<Cert> {
   // we'll skip loading settings in jest-land
   if (process.env.JEST_WORKER_ID !== undefined) {
-    const loaded = global as unknown as { secretAgentCert: Promise<Cert> };
+    const loaded = (global as unknown) as { secretAgentCert: Promise<Cert> };
 
     if (!loaded.secretAgentCert) {
       loaded.secretAgentCert = generateCert();
