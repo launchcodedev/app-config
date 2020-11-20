6ab1b49a339111584365eb1eb799fd486f0f21af
Joel Gallant <joel@joelgallant.me>
2020-11-19 18:56:21 -0700

    feat: adds an 'app-config-secret-agent' bin, exclusively for convenience

---
+++ app-config/src/start-secret-agent.ts
@@ -0,0 +1,12 @@
+import { startAgent } from './secret-agent';
+import { logger } from './logging';
+
+startAgent().catch((error: unknown) => {
+  if (error instanceof Error) {
+    logger.error(`Error: ${error?.toString()}`);
+  } else {
+    logger.error('An unknown error occurred');
+  }
+
+  process.exit(1);
+});
