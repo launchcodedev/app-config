dc1e1e8772b69fe9d84721e688d525f65074a6f8
Joel Gallant <joel@joelgallant.me>
2020-12-09 13:13:15 -0700

    feat: enables rust language compliance via code generation

--- app-config/src/generate.ts
+++ app-config/src/generate.ts
@@ -92,6 +92,15 @@ export async function generateQuicktype(
     });
   }
 
+  if (['rs', 'rust'].includes(type)) {
+    leadingComments = [];
+    Object.assign(rendererOptions, {
+      'derive-debug': 'true',
+      'edition-2018': 'true',
+      ...rendererOptions,
+    });
+  }
+
   const { lines } = await quicktype({
     inputData,
     lang: type,
@@ -202,6 +211,77 @@ export async function generateQuicktype(
         return '\t'.repeat(match.length / 2);
       }),
     );
+  }
+
+  if (['rs', 'rust'].includes(type)) {
+    const imports = ['use serde_derive::{Deserialize, Serialize};', 'use valico::json_schema;'];
+
+    const loadConfig = [
+      '#[derive(Debug)]',
+      'pub enum Error {',
+      "    EnvironmentVariableNotFound(&'static str),",
+      '    JsonParsing(serde_json::Error),',
+      '    Validation(json_schema::ValidationState),',
+      '}',
+      '',
+      'pub fn load_config() -> Result<Config, Error> {',
+      '    let config_text = match std::env::var("APP_CONFIG") {',
+      '        Ok(text) => text,',
+      '        Err(_) => {',
+      '            return Err(Error::EnvironmentVariableNotFound("APP_CONFIG"));',
+      '        }',
+      '    };',
+      '',
+      '    let schema_text = match std::env::var("APP_CONFIG_SCHEMA") {',
+      '        Ok(text) => text,',
+      '        Err(_) => {',
+      '            return Err(Error::EnvironmentVariableNotFound("APP_CONFIG_SCHEMA"));',
+      '        }',
+      '    };',
+      '',
+      '    let config = serde_json::from_str(&config_text).map_err(Error::JsonParsing)?;',
+      '    let schema = serde_json::from_str(&schema_text).map_err(Error::JsonParsing)?;',
+      '',
+      '    let mut scope = json_schema::Scope::new();',
+      '    let schema = scope.compile_and_return(schema, false).unwrap();',
+      '    let result = schema.validate(&config);',
+      '',
+      '    if !result.is_valid() {',
+      '        return Err(Error::Validation(result));',
+      '    }',
+      '',
+      '    return serde_json::from_value(config).map_err(Error::JsonParsing);',
+      '}',
+      '',
+      'impl std::error::Error for Error {}',
+      '',
+      'impl std::fmt::Display for Error {',
+      "    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {",
+      '        match self {',
+      '            Error::EnvironmentVariableNotFound(var) => {',
+      '                write!(fmt, "EnvironmentVariableNotFound({})", var)?;',
+      '            }',
+      '            Error::JsonParsing(error) => {',
+      '                write!(fmt, "JSON Parsing Error: {}", error)?;',
+      '            }',
+      '            Error::Validation(state) => {',
+      '                write!(fmt, "JSON Schema Validation Error: {:?}", state)?;',
+      '            }',
+      '        }',
+      '',
+      '        Ok(())',
+      '    }',
+      '}',
+    ];
+
+    for (const [ind, line] of lines.entries()) {
+      lines[ind] = line.replace(/^( {2})/g, '    ');
+    }
+
+    const importsIndex = lines.findIndex((line) => line.startsWith('use serde::'));
+
+    lines.splice(importsIndex, 1, ...imports);
+    lines.splice(lines.length, 0, ...loadConfig);
   }
 
   return lines;
