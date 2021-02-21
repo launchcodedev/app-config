9ebde2adb1f9ff89e9ad7eb2f24fd7ff25b77e11
Joel Gallant <joel@joelgallant.me>
2021-02-20 22:07:08 -0700

    feat: uses extension-utils for git and vault parsing extensions

--- app-config-git/src/index.ts
+++ app-config-git/src/index.ts
@@ -1,17 +1,16 @@
 import simpleGit from 'simple-git';
 import { ParsingExtension, AppConfigError } from '@app-config/core';
+import { forKey, validateOptions } from '@app-config/extension-utils';
 
 /** Access to the git branch and commit ref */
 export default function gitRefDirectives(
   getStatus: typeof gitStatus = gitStatus,
 ): ParsingExtension {
-  return (value, [_, key]) => {
-    if (key === '$git') {
-      return async (parse) => {
-        if (typeof value !== 'string') {
-          throw new AppConfigError('$git directive should be passed a string');
-        }
-
+  return forKey(
+    '$git',
+    validateOptions(
+      (SchemaBuilder) => SchemaBuilder.stringSchema(),
+      (value) => async (parse) => {
         switch (value) {
           case 'commit':
             return getStatus().then(({ commitRef }) => parse(commitRef, { shouldFlatten: true }));
@@ -41,11 +40,9 @@ export default function gitRefDirectives(
           default:
             throw new AppConfigError('$git directive was not passed a valid option');
         }
-      };
-    }
-
-    return false;
-  };
+      },
+    ),
+  );
 }
 
 interface GitStatus {
