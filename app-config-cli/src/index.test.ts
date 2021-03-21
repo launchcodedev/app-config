836b42c616f3638e87a0c4eabb135951bdba3a87
Joel Gallant <joel@joelgallant.me>
2021-03-21 17:10:59 -0600

    ci: temp

--- app-config-cli/src/index.test.ts
+++ app-config-cli/src/index.test.ts
@@ -86,7 +86,7 @@ describe('vars', () => {
     expect(stdout).toMatchSnapshot();
   });
 
-  it('prints only nonSecrets without --secrets option', async () => {
+  it.only('prints only nonSecrets without --secrets option', async () => {
     await withTempFiles(
       {
         '.app-config.yml': `
