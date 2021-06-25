259d9ce549eee10d386c56117b2f0f16d72e6702
Joel Gallant <joel@joelgallant.me>
2021-06-25 17:30:07 -0600

    test: clears stringProperty

--- examples/vite-project/cypress/integration/main.spec.js
+++ examples/vite-project/cypress/integration/main.spec.js
@@ -4,29 +4,25 @@ describe('Config Loading', () => {
   it('should render the configuration', () => {
     cy.visit('/');
 
-    cy.get('body').should('contain', `"urlProperty": "https://example.com"`);
     cy.get('body').should(
       'contain',
-      `"longStringProperty": "some long string with a \\" char and '\\\\n"`,
+      `"stringProperty": "some long string with a \\" char and '\\\\n"`,
     );
   });
 
   it('should mock the configuration value', () => {
     cy.setAppConfig({
-      urlProperty: 'https://overwritten.com',
-      longStringProperty: 'shorter than before',
+      stringProperty: 'shorter than before',
     });
 
     cy.visit('/');
 
-    cy.get('body').should('contain', `"urlProperty": "https://overwritten.com"`);
-    cy.get('body').should('contain', `"longStringProperty": "shorter than before"`);
+    cy.get('body').should('contain', `"stringProperty": "shorter than before"`);
   });
 
   it('should fail when overriden with an invalid value', () => {
     cy.setAppConfig({
-      urlProperty: 42,
-      longStringProperty: 'short!',
+      stringProperty: 42,
     });
 
     cy.visit('/');
