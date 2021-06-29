48e57e39f50e5542529bcdf199b0f5fdf9f19406
John Brandt <johnb0@outlook.com>
2021-06-28 20:47:56 -0600

    fix: lints

--- app-config-electron/src/preloader.ts
+++ app-config-electron/src/preloader.ts
@@ -1,8 +1,10 @@
 import { contextBridge } from 'electron';
+import { inspect } from 'util';
 import { logger } from '@app-config/logging';
+import type { ExportedConfig } from '@app-config/main';
 
 let additionalPreload: string | undefined;
-let config;
+let config: ExportedConfig | undefined;
 
 for (const arg of process.argv) {
   if (arg.startsWith('--user-preload=')) {
@@ -11,15 +13,17 @@ for (const arg of process.argv) {
 
   if (arg.startsWith('--app-config=')) {
     try {
-      config = JSON.parse(arg.substr(13));
+      config = JSON.parse(arg.substr(13)) as ExportedConfig;
     } catch (err) {
-      logger.error(`Got invalid JSON from config: ${err}`);
+      logger.error(`Got invalid JSON from config: ${inspect(err)}`);
     }
   }
 }
 
-contextBridge.exposeInMainWorld('_appConfig', config);
-logger.info(`⚙️ Injected app-config`);
+if (config) {
+  contextBridge.exposeInMainWorld('_appConfig', config);
+  logger.info(`⚙️ Injected app-config`);
+}
 
 // This seems to be how electron does preload scripts https://github.com/electron/electron/issues/2406 maybe there's a better way?
 /* eslint-disable import/no-dynamic-require, global-require */
