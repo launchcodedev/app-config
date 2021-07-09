4554bf1a88a7d4d1d67b2068f72f3f12a1c8e74f
Greg Richardson <greg@lc.dev>
2021-07-09 10:45:37 -0500

    fix: react native config export

--- app-config-react-native/src/index.ts
+++ app-config-react-native/src/index.ts
@@ -1,4 +1,5 @@
 import { loadValidatedConfig } from '@app-config/main';
+import { currentEnvironment } from '@app-config/node';
 import { relative } from 'path';
 import { Transformer, upstreamTransformer } from './upstream-transformer';
 
@@ -23,7 +24,26 @@ export const transform: Transformer['transform'] = async ({ src, filename, optio
   // Parse config and overwrite app-config's entry point with exported config JSON
   const { fullConfig } = await loadValidatedConfig();
 
-  const modifiedSrc = `module.exports = ${JSON.stringify(fullConfig)};`;
+  const modifiedSrc = generateSrc(JSON.stringify(fullConfig));
 
   return upstreamTransformer.transform({ src: modifiedSrc, filename, options });
+};
+
+const generateSrc = (config: string) => {
+  let generatedText: string;
+
+  generatedText = `
+    const config = ${config};
+
+    export { config };
+    export default config;
+  `;
+
+  generatedText = `${generatedText}
+    export function currentEnvironment() {
+      return ${JSON.stringify(currentEnvironment()) ?? 'undefined'};
+    }
+  `;
+
+  return generatedText;
 };
