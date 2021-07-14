db7d5ac36b504fe73be393609d59e9a106af6c38
John Brandt <johnb0@outlook.com>
2021-07-13 20:45:55 -0600

    test: add electron tests

--- examples/electron-project/src/main.ts
+++ examples/electron-project/src/main.ts
@@ -1,12 +1,13 @@
 import { app, BrowserWindow } from 'electron';
 import { addAppConfigPreload } from '@app-config/electron';
 import { loadConfig, config } from '@app-config/main';
+import path from 'path';
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: addAppConfigPreload(config),
+    webPreferences: addAppConfigPreload(config, { preload: process.env.NODE_ENV === 'test' ? path.join(__dirname, 'preload-test.js') : undefined }),
   });
 
   mainWindow.loadFile('./index.html')
