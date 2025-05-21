7269ceaf838fb40abb40ba444f0c695db565bfb3
John Brandt <johnb0@outlook.com>
2025-05-21 16:27:45 -0600

    feat: add aws secrets manager plugin

---
+++ app-config-aws-secrets-manager/src/index.ts
@@ -0,0 +1,42 @@
+import type { ParsingExtension, Json } from '@app-config/main';
+import { named, forKey, validateOptions } from '@app-config/extension-utils';
+import { SecretsManager } from '@aws-sdk/client-secrets-manager';
+
+function awsSecretsManagerParsingExtension(): ParsingExtension {
+  return named(
+    '$awsSecretsManager',
+    forKey(
+      '$awsSecretsManager',
+      validateOptions(
+        (SchemaBuilder) =>
+          SchemaBuilder.emptySchema().addString('secretId').addString('select', {}, false),
+        ({ secretId, select }) =>
+          async (parse) => {
+            const client = new SecretsManager();
+            const secret = await client.getSecretValue({ SecretId: secretId });
+
+            if (!secret.SecretString) {
+              throw new Error(`Secret ID ${secretId} not found in AWS Secrets Manager`);
+            }
+
+            let value = secret.SecretString as Json;
+
+            if (select) {
+              const json = JSON.parse(secret.SecretString) as Record<string, Json>;
+              value = json[select];
+
+              if (!value) {
+                throw new Error(
+                  `Could not select ${select} JSON key from Secret ID ${secretId} in AWS Secrets Manager`,
+                );
+              }
+            }
+
+            return parse(value, { shouldFlatten: true });
+          },
+      ),
+    ),
+  );
+}
+
+export default awsSecretsManagerParsingExtension;
